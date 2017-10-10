5c183bff4e5556a02a5c72fb058227def32b68c3
Inder <inder@oad.earth>
2017-10-10 10:48:22 +0200

    Update phi1612.h

--- phi1612.h
+++ phi1612.h
@@ -5,7 +5,9 @@
 extern "C" {
 #endif
 
-void phi1612_hash(const char* input, char* output);
+#include <stdint.h>
+
+void phi1612_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
 }
